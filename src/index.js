e83a67f711a041b753e483d1c1a510e747cc8b50
MphoMN <mphondou018@gmail.com>
2025-02-08 15:10:04 +0200

    Added basic form handling and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateAffirmation(event) {
+  event.preventDefault();
 
+  new Typewriter("#affirm", {
+    strings: "Today is filled with endless opportunities",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+let affirmFormElement = document.querySelector("#affirm-gen");
+affirmFormElement.addEventListener("submit", generateAffirmation);
